12a039c5120d18a321680c92ca90ad2c2d86b879
rabdiego <diegorabelodesa@gmail.com>
2023-12-09 14:59:12 -0300

    Commented on some code

--- libs/dino/include/obstacle.hpp
+++ libs/dino/include/obstacle.hpp
@@ -52,12 +52,51 @@ private:
 
 public:
   sf::Rect<float> rect;
+
+  /**
+  * Class constructor
+  * 
+  * The id will determine which type of obstacle the object is, being:
+  * 0 - Small Cactae
+  * 1 - Medium Cactae
+  * 2 - Large Cactae
+  * 3 - Pterossaurus
+  *
+  * @param id The object id
+  * @param floorHeight The y coordinate of the game's floor
+  * @param skyHeight The height in which the pterossaurus objects will be above the ground
+  *
+  */
   Obstacle(int id, int floorHeight, int skyHeight);
 
+  /**
+  * Update method
+  *
+  * @param gameVelocity Current game velocity
+  */
   void update(float gameVelocity);
+
+  /**
+  * Draw method
+  *
+  * @param window Game window
+  * @param frame Current frame
+  */
   void draw(sf::RenderWindow &window, int frame);
+
+  /**
+  * Method used to check collision with a Dino class object
+  *
+  * @param dino Dino object
+  * @returns boolean telling if the obstacle collided with the dino
+  */
   bool collidedWithDino(DinoAI::Dino dino);
 
+  /**
+  * Method used to get the obstacle's position
+  *
+  * @returns the obstacle's position
+  */
   sf::Vector2f getPosition();
 };
 }; // namespace DinoAI
